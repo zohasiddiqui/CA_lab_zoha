cfd6717b6264f8a9c7cea5655caa535684c8a8a1
zohasiddiqui <zs11520@st.habib.edu.pk>
2026-09-07 15:40:52 +0500

    added task 4

--- lab01/task4.s
+++ lab01/task4.s
@@ -1,14 +1,24 @@
 # task 4a
-.text
-.globl main 
-main:
+
 .text
 .globl main 
 main:    
     li x10,0x78786464
     li x11, 0xA8A81919
-    li x21,0x100
+
+    li x21, 0x100
+    li x22, 0x1F0
+
     sw x10, 0(x21)
-1
+
+    sw x11, 0(x22)
+
+    lhu x12, 0(x21)
+
+    lh x13, 0(x22)
+
+    lb x14, 0(x22)
+
+
 end:
     j end
